72893b0c6a744236bac5c41d3505a96ad77d6fcc
zungu <siyabongazungu666@gmail.com>
2026-03-19 02:05:34 +0200

    PlaceNameBST.class

---
+++ PlaceNameBST.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+
+public class PlaceNameBST {
+   public BinaryTreeNode<PlaceNameEntry> root;
+   public int comparisonCounter;
+   
+   public PlaceNameBST(PlaceNameEntry root, int comparisonCounter)   {
+      root = null;
+   }
+   
+   public PlaceNameBST() {
+      this.root = null;
+      this.comparisonCounter = 0;
+   }
+   public void insert(PlaceNameEntry data)   {
+      root = insert(data, root );
+      
+   }
+   
+   public BinaryTreeNode<PlaceNameEntry> insert (PlaceNameEntry d, BinaryTreeNode<PlaceNameEntry> node )
+  {
+      if (node == null) {
+         return new BinaryTreeNode<PlaceNameEntry>(d, null, null);
+      }
+      if(d.getPlaceName().compareTo(node.data.getPlaceName()) == 0){
+         return node;
+      }
+      if (d.getPlaceName().compareTo(node.data.getPlaceName()) < 0)
+      {
+        node.left = insert(d, node.left);
+      }
+      else
+      {
+         node.right = insert(d, node.right);
+      }
+      
+      return node;
+   }
+
+   public void load(String filename, int N)  {
+      File file = new File(filename);
+      
+      try(Scanner scanner = new Scanner(file)){
+         
+         scanner.nextLine();
+         int count = 0;
+         while(scanner.hasNextLine()){
+            
+            if(count >= N) {
+               break;
+            }
+            //count++;
+            
+            String line = scanner.nextLine();
+            
+            String[] parts = line.split(",");
+            int id = Integer.parseInt(parts[0]);
+            String placeName = parts[1];
+            String municipality = parts[2];
+            String province = parts[3];
+            String population = parts[4];
+            
+            PlaceNameEntry object = new PlaceNameEntry(id, placeName, municipality, province, population);
+            
+            insert(object);
+            
+            count++;
+            System.out.println("Inserted: " + placeName);
+         }
+      } catch (FileNotFoundException error) {
+         System.out.println("File not found.");
+            
+      }
+   }
+   
+   public PlaceNameEntry find(String placeName, BinaryTreeNode<PlaceNameEntry> node) {
+      if (node == null) {
+         return null; 
+      }
+      int result = placeName.compareTo(node.data.getPlaceName());
+      comparisonCounter++;
+   
+      if (result == 0) {
+         return node.data; 
+      } else if (result < 0) {
+         return find(placeName, node.left);  
+      } else {
+         return find(placeName, node.right); 
+      }
+   }
+   
+   public PlaceNameEntry search(String placeName) {
+      comparisonCounter = 0;
+      return find(placeName, root);
+   }
+   
+   public int getComparisonCount() {
+      return comparisonCounter;
+   }
+}
